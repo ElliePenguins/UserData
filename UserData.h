9f4a5208f09b1ecddbd75d3eec20f7ea7a6a5339
ElliePenguins <34112606+ElliePenguins@users.noreply.github.com>
2020-01-27 19:09:16 -0500

    Modified lib Header.

---
+++ UserData.h
@@ -0,0 +1,18 @@
+/* 
+ * Top level header that includes all the other
+ * headers for actual use of this library.
+ *
+ * MIT License
+ * Copyright (c) 2020 ElliePenguins
+ */
+
+#ifndef USER_DATA_H_
+#define USER_DATA_H_
+
+#include <UserData/format.h>
+#include <UserData/userProfile.h>
+#include <UserData/userInteraction.h>
+#include <UserData/userQuery.h>
+
+#endif 
+
